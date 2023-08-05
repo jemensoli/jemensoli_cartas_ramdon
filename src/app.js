ec69e42c668fb10a659b1917194db26cff87b4f1
jemensoli <128940851+jemensoli@users.noreply.github.com>
2023-08-05 23:38:06 +0000

    Version 1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generarCartaAleatoria();
+  document
+    .querySelector(".generarCarta")
+    .addEventListener("click", generarCartaAleatoria);
 };
+
+function generarCartaAleatoria() {
+  let numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "J",
+    "Q",
+    "K",
+    "AS"
+  ];
+  //write your code here
+  let simbolos = [
+    { simbolo: "♦", color: "red" },
+    { simbolo: "♥", color: "red" },
+    { simbolo: "♠", color: "black" },
+    { simbolo: "♣", color: "black" }
+  ];
+
+  let numeroCarta = numeros[Math.floor(Math.random() * numeros.length)];
+  document.querySelector(".numeroCarta").innerHTML = numeroCarta;
+
+  let simboloCarta = simbolos[Math.floor(Math.random() * simbolos.length)];
+
+  let simboloSuperiorCarta = document.querySelector(".simboloCartaSuperior");
+  let simboloInferiorCarta = document.querySelector(".simboloCartaInferior");
+
+  simboloSuperiorCarta.innerHTML = simboloCarta.simbolo;
+  simboloInferiorCarta.innerHTML = simboloCarta.simbolo;
+
+  if (simboloCarta.color == "red") {
+    simboloSuperiorCarta.style.color = "red";
+    simboloInferiorCarta.style.color = "red";
+  } else {
+    simboloSuperiorCarta.style.color = "black";
+    simboloInferiorCarta.style.color = "black";
+  }
+}
